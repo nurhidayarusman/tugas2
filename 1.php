b5a8820c6ad73cb9c2374905003d15b00cb4822f
nurhidayarusman <59651820+nurhidayarusman@users.noreply.github.com>
2020-01-08 22:03:23 +0800

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,79 @@
+<html>
+	<head>
+		<title>pesona indonesia</title>
+	</head>
+	<body bgcolor="LIGHTSKYBLUE">
+		<table align="center" border="1" style="width:775px;">
+			<tr>
+				<th colspan="4" height="177px" width="775px" background="bg.jpg" ><h2><font color="white">PESONA ALAM INDONESIA</font></h2>
+																			
+				</th>
+			</tr>
+			<tr>
+				<td align="center" colspan="4" height="40px">
+					<table width="766px" height="40px">
+						<tr bgcolor="hotpink">
+							<td><b><center><a href="tugasTabel.html">Beranda</a></center></b></td>
+							<td><b><center><a href="Artikel.html">Artikel</a></center></b></td>
+							<td><b><center><a href="Berita.Html">Berita</a></center></b></td>
+					</table>
+				</td>
+			<tr>
+				<td valign="top" colspan="3" rowspan="4" bgcolor="pink">
+				<center><h3><b>7 PENGINAPAN DIATAS LAUT</b></h3></center>
+				<p><dd>Pecinta pantai pasti tak ingin jauh-jauh dari air. Wisatawan yang begini biasanya akan memilih hotel yang
+		langsung menghadap ke pantai. Hmm, rasanya seru ya saat buka pintu langsung disuguhi indahnya laut dan
+		birunya langit. Ditambah lagi aroma laut yang membuat hawa liburan semakin terasa. Saat sore hari pun hanya
+		perlu duduk cantik di depan kamar untuk menunggu menguningnya langit karena senja. Hmm, jadi ingin liburan,
+		kan.Tapi, ada yang lebih seru nih dari sekedar kamar langsung menghadap pantai. Adalah penginapan yang 
+		menyediakan kamar langsung di atas laut, alias terapung. Hmm, kebayang kan serunya tidur dengan suara ombak
+		yang terdengar jelas? Ditambah lagi, setelah buka pintu bisa langsung nyemplung. Mau berenang atau snorkling?
+		Bebas! Di mana saja, sih?</p>
+		<h4 align="left">
+			<li>RAJA AMPAT</li>
+			<li>PAPUA PARADISE ECO RESORT</li>
+			<li>ORA BEACH RESORT (MALUKU)</li>
+			<li>PULAU CUBADAK (PADANG)</li>
+			<li>MARATUA PARADISE RESORT</li>
+			<li>TELUNAS RESORT PRIVATE ISLAND</li>
+		</ol>
+		</h4>
+	    <b>Gambar</b>
+			<br><img src="raja.jpeg" width="250px" height="100px">
+			<ul>
+				<li> Download Gambar dengan mengklik icon <a href="raja.jpeg" target="_blank" download="raja.jpeg  ">
+														  <img src="ikon.jpg" width="50px" height="50px"></a></li>
+			</ul>
+				<b>Video</b>
+			<br><video width="300px" height="150px" controls>
+			<source src="video.mp4"> 
+		</video>
+				 <h4 align="left">Download video dengan mengklik icon</h4>
+				 <a href="video.mp4" target="_blank" download="video.mp4  ">
+														  <img src="ikon.jpg" width="50px" height="50px"></a>
+				</td>
+			</tr>
+			<tr>
+				<td align="center" height="950px" width="250px" bgcolor="lavender" valign="top">
+					<b>BERITA YANG BELUM TERBUKA</b>
+					<h4 align="left">
+					<ul>
+						<li>Berita Kedua</li>
+						<a href="2.html">Pantai Kuta<a>
+						<p><li>Berita Ketiga</li>
+						<a href="3.html">Menikmati Kesunyian Alam Tropis di Private Resort Pulau Pangkil</a></p>
+					</h4>
+				</td>
+			</tr>
+			<tr>
+			</tr>
+			<tr>	
+			</tr>
+			<tr>
+				<td align="center" border="1px" colspan="4" height="1px" width="800px" bgcolor="hotpink">
+					<font size="3">Nurhidaya Rusman (216280047)</font>
+				</td>
+			</tr>
+		</table>
+	</body>					
+</html>
---
+++ 2.php
@@ -0,0 +1,71 @@
+<html>
+	<head>
+		<title>pesona indonesia</title>
+	</head>
+	<body bgcolor="LIGHTSKYBLUE">
+		<table align="center" border="1" style="width:775px;">
+			<tr>
+				<th colspan="4" height="177px" width="775px" background="bg.jpg" ><h2><font color="white">PESONA ALAM INDONESIA</font></h2>
+																			
+				</th>
+			</tr>
+			<tr>
+				<td align="center" colspan="4" height="40px">
+					<table width="766px" height="40px">
+						<tr bgcolor="hotpink">
+							<td><b><center><a href="tugasTabel.html">Beranda</a></center></b></td>
+							<td><b><center><a href="Artikel.html">Artikel</a></center></b></td>
+							<td><b><center><a href="Berita.Html">Berita</a></center></b></td>
+					</table>
+				</td>
+			<tr>
+				<td  colspan="3" rowspan="4" bgcolor="pink" valign="top">
+								<center><b><h3>PANTAI KUTA</h3></b></center>
+					<p><dd>Pantai Kuta adalah sebuah tempat pariwisata yang terletak kecamatan Kuta, sebelah selatan Kota Denpasar, Bali,
+                       Indonesia. Daerah ini merupakan sebuah tujuan wisata turis mancanegara dan telah menjadi objek wisata andalan Pulau
+                       Bali sejak awal tahun 1970-an. Pantai Kuta sering pula disebut sebagai pantai matahari terbenam (sunset beach)
+                       sebagai lawan dari pantai Sanur. Selain itu, Lapangan Udara I Gusti Ngurah Rai terletak tidak jauh dari Kuta.</p>
+					<p><dd>Sebelum menjadi objek wisata, Kuta merupakan sebuah pelabuhan dagang tempat produk lokal diperdagangkan kepada 
+                       pembeli dari luar Bali. Pada abad ke-19, Mads Lange, seorang pedagang Denmark, datang ke Bali dan mendirikan basis
+                       perdagangan di Kuta. Ia ahli bernegosiasi sehingga dirinya terkenal di antara raja-raja Bali dan Belanda.</p>
+					<p><dd>Pantai Kuta terkenal memiliki ombak yang bagus untuk olahraga selancar (surfing)[2], terutama bagi peselancar
+                       pemula. Selain keindahan pantai, wisata pantai Kuta juga menawarkan berbagai jenis hiburan seperti bar, restoran,
+                       pertokoan, restoran, hotel, dan toko-toko kelontong, serta pedagang kaki lima di sepanjang pantai menuju pantai
+                       Legian.</p>
+                    <p><dd>Pantai Kuta dapat ditempuh dengan waktu sekitar 10 menit dari Bandara Internasional Ngurah Rai dalam kondisi 
+                       jalanan lancar.Sebagai tempat wisata pantai, pantai Kuta dilengkapi lahan parkir di sepanjang pantai, kamar mandi 
+                       umum, payung pantai, kios makanan dan minuman, serta tempat penyewaan papan selancar.Setiap tahun, pengunjung pantai 
+                       Kuta kerap mengeluhkan masalah kebersihan dan tumpukan sampah di pantai Kuta, terutama saat musim liburan. Hal 
+                       tersebut mempengaruhi penilaian wisatawan domestik maupun manca negara terhadap citra pantai Kuta.Selain disebabkan
+                       aktivitas pengunjung dan penjual di sepanjang pantai Kuta, sampah-sampah di pantai Kuta juga diakibatkan hembusan
+                       angin barat setiap tahunnya yang membawa sampah dari muara-muara sungai terdekat ke pantai.Permasalahan ini berusaha
+                       diatasi oleh prajuru Desa Adat Kuta dan anggota Badan Penyelamat Wisata Tirta (Balawista) yang merupakan mitra dari
+                       Dinas Kebersihan dan Pertamanan Kabupaten Badung. Setiap pagi, Dinas Kebersihan dan Pertamanan juga aktif mengoperasi
+                       kan mobil loader untuk memunguti sampah di pagi hari.[6][7] Permasalahan ini juga memperoleh perhatian utama dari TNI
+                       , berbagai organisasi masyarakat, dan industri-industri pariwisata yang berada di wilayah Pantai Kuta.</p>
+				</td>
+			</tr>
+			<tr>
+				<td align="center" height="850px" width="250px" bgcolor="lavender" valign="top">
+					<b>BERITA YANG BELUM TERBUKA</b>
+					<h4 align="left">
+					<ul>
+						<li>Berita Pertama</li>
+						<a href="1.html">7 Penginapan Diatas Laut<a>
+						<p><li>Berita Ketiga</li>
+						<a href="3.html">Menikmati Kesunyian Alam Tropis di Private Resort Pulau Pangkil</a></p>
+					</h4>
+				</td>
+			</tr>
+			<tr>
+			</tr>
+			<tr>	
+			</tr>
+			<tr>
+				<td align="center" border="1px" colspan="4" height="1px" width="800px" bgcolor="hotpink">
+					<font size="3">Nurhidaya Rusman (216280047)</font>
+				</td>
+			</tr>
+		</table>
+	</body>					
+</html>
